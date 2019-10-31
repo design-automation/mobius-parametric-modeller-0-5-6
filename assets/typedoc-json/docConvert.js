d3f77e676fde1dfcdaa80e0119f75fa6d5ccc1fd
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2019-10-31 12:44:55 +0800

    update

--- assets/typedoc-json/docConvert.js
+++ assets/typedoc-json/docConvert.js
@@ -32,7 +32,7 @@ if (examples === undefined) {
         "name": "Function Examples",
         "files": [
         ],
-        "link": "https://raw.githubusercontent.com/design-automation/mobius-parametric-modeller/master/src/assets/gallery/function_examples/"
+        "link": "https://raw.githubusercontent.com/design-automation/mobius-parametric-modeller-0-5-6/master/assets/gallery/function_examples/"
     };
     config.data.push(examples);
 }
